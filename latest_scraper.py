32f197ab59d0ddcc2e54ceb89a81c2d49bcf5a1a
Obaid26 <51287329+Obaid26@users.noreply.github.com>
2021-05-11 09:20:52 -0700

    frontend

--- latest_scraper.py
+++ latest_scraper.py
@@ -5,11 +5,6 @@ import json
 from modules.requests import get
 from modules.db import DB
 
-interval_map = dict(
-    m="minutes",
-    h="hours",
-    d="days",
-)
 chain_map = dict(
     bscTokens="bsc",
     ethTokens="eth"
@@ -18,6 +13,7 @@ while True:
     db = DB("data/db.db")
     
     res = get("https://tokensniffer.com/tokens/new",wait=10*60)
+    
     soup = BeautifulSoup(res.text,"lxml")
     script_content = soup.select("script#__NEXT_DATA__")[0].string
     data = json.loads(script_content)["props"]["pageProps"]
@@ -26,19 +22,31 @@ while True:
         tokens = data[chain]
         for token in tokens:
             added = datetime.strptime(token["created_at"],"%Y-%m-%dT%H:%M:%S.%fZ")
-            obj = dict(
+            latest = dict(
                 address=token["addr"],
                 name=token["name"],
                 symbol=token["symbol"].upper(),
                 chain=chain_map[chain],
-                defunct=int(token["defunct"]),
-                source_md5=token.get("source_md5",""),
                 added=added.timestamp()
             )
-            if obj["source_md5"] is None:
-                obj["source_md5"] = ""
-            
-            print(obj["source_md5"])
-            db.insert("latest",obj,commit=False,ignore_insert=False)
-    print("Updated")    
+            bscheck = dict(
+                address=token["addr"],
+                rating="",
+                burned_tokens=0,
+                total_supply=0,
+                holders=0,
+            )
+            tokensniffer = dict(
+                address=token["addr"],
+                verified_source=True,
+                proxy_contains=True,
+                pausable_contains=True,
+                defunct=int(token["defunct"]),
+                source_md5=token["source_md5"] if token["source_md5"] is not None else "",
+            )
+            db.insert("latest",obj,commit=False,ignore_insert=True)
+            db.insert("bscheck",obj,commit=False,ignore_insert=True)
+            db.insert("tokensniffer",obj,commit=False,ignore_insert=True)
+    
+    print("Updated")
     db.close()
--- scripts/setup.py
+++ scripts/setup.py
@@ -30,5 +30,33 @@ CREATE TABLE latest (
     added INTEGER NOT NULL
 )
 """
+a += """
+CREATE TABLE bscheck (
+    address TEXT PRIMARY KEY NOT NULL ,
+    rating TEXT NOT NULL ,
+    burned_tokens INTEGER NOT NULL,
+    total_supply INTEGER NOT NULL,
+    holders INTEGER NOT NULL,
+)
+"""
+a += """
+CREATE TABLE top_holders (
+    address TEXT PRIMARY KEY NOT NULL ,
+    is_dev INTEGER NOT NULL,
+    percentage FLOAT NOT NULL,
+)
+"""
+a += """
+CREATE TABLE tokensniffer (
+    address TEXT PRIMARY KEY NOT NULL ,
+    verified_source INTEGER NOT NULL ,
+    prior_similar INTEGER NOT NULL ,
+    proxy_contains INTEGER NOT NULL ,
+    pausable_contains INTEGER NOT NULL ,
+    defunct INTEGER NOT NULL ,
+    source_md5 STRING NOT NULL ,
+    deployed INTEGER NOT NULL
+)
+"""
 
 a.execute()
