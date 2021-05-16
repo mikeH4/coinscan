75dd0d315306440f3102a4a07054759f4c5694a3
Obaid26 <51287329+Obaid26@users.noreply.github.com>
2021-05-16 11:39:48 -0700

    Snap

--- api.py
+++ api.py
@@ -4,18 +4,15 @@ from fastapi.middleware.cors import CORSMiddleware
 from api_modules import v1
 
 app = FastAPI(openapi_url=None)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+)
 
 routers = [
     v1.app,
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["coinscan.finance"],
-    allow_credentials=True,
-    allow_methods=["GET","OPTIONS"],
-    allow_headers=["DNT","X-Mx-ReqToken","Keep-Alive","User-Agent","X-Requested-With","If-Modified-Since","Cache-Control","Content-Type","X-Api-Auth"],
-)
 
 @app.get("/")
 async def root():
