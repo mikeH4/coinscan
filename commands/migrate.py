f988dcbedaf7993f83dcaa8df528299a76671f52
Obaid26 <51287329+Obaid26@users.noreply.github.com>
2021-05-16 23:15:37 -0700

    Edits for better syntax, better migration

--- commands/migrate.py
+++ commands/migrate.py
@@ -1,11 +1,13 @@
 from library.sqlite import DB
+from library.postgres import DB as _DB
 from core.Token import Token
 
 db = DB("data/tokens.db")
+_db = _DB("tokens")
 
 rows = db.get_all("SELECT * FROM tokens")
 for row in rows:
     kwargs = {key:row[i] for i,key in enumerate(Token.keys)}
-    Token.insert_or_update(Token(**kwargs),db)
+    Token.insert_or_update(Token(**kwargs),_db)
 
-db.conn.commit()
+_db.close()
--- core/CoreToken.py
+++ core/CoreToken.py
@@ -33,36 +33,12 @@ class CoreToken:
         not_pausable:bool,
 
     ) -> None:
-        
-        self.name = name
-        self.symbol = symbol
-        self.address = address
-        self.block_time = block_time
-        self.updated = updated
+        lcl = locals()
+        for key,_class in CoreToken.__init__.__annotations__.items():
+            if key == "return":
+                continue
+            setattr(self,key,_class(lcl[key]))
 
-        # BscScan
-        self.total_supply = total_supply
-        self.decimals = decimals
-        self.source_verified = source_verified
-
-        # BscCheck
-        self.rating = rating
-        self.honeypot_check = honeypot_check
-        self.owner_renounced = owner_renounced
-        self.dev_liquidity_check = dev_liquidity_check
-        self.lp_check = lp_check
-        self.top_holders_check = top_holders_check
-
-
-        # TokenSniffer
-        self.deployed = deployed
-        self.first_seen = first_seen
-        self.source_md5 = source_md5
-        self.similar_count = similar_count
-        self.similar_viewable = similar_viewable
-        self.no_older_tokens = no_older_tokens
-        self.not_proxy = not_proxy
-        self.not_pausable = not_pausable
 
     keys = [
         "address",
