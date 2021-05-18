317c44a1ab56c03b4cdfcc161b0d141c5a0bf997
Obaid26 <51287329+Obaid26@users.noreply.github.com>
2021-05-17 18:51:56 -0700

    Automatic re-adding, so we don't depend on a cron

--- core/TokenPuller.py
+++ core/TokenPuller.py
@@ -1,3 +1,4 @@
+from time import time
 from core.sources.BSCheck import BSCheck
 from core.sources.BscScan import BscScan
 from core.sources.TokenFomo import TokenFomo
@@ -31,49 +32,58 @@ class TokenPuller:
 
         self.db = DB("tokens")
 
-        data = tokenfomo.get()
+        while True:
+            cycle_start = time()
+            data = tokenfomo.get()
 
-        existing_addrs = [] if not ignore_existing else self.get_existing_addresses(
-            [row["addr"] for row in data],
-            # In last 30 min
-            updated_after=(
-                int(datetime.now().timestamp()-(60*60*2))
-                if ignore_existing == "recent"
-                else None
+            existing_addrs = [] if not ignore_existing else self.get_existing_addresses(
+                [row["addr"] for row in data],
+                # In last 30 min
+                updated_after=(
+                    int(datetime.now().timestamp()-(60*60*2))
+                    if ignore_existing == "recent"
+                    else None
+                )
             )
-        )
-        data_len = len(data)
-        
-        for i,record in enumerate(data):
-            if record["chainId"] != "BSC":
-                continue
-            if record["addr"] in existing_addrs:
-                print("Skipped:",record["addr"])
-                continue
+            data_len = len(data)
+            
+            for i,record in enumerate(data):
+                if record["chainId"] != "BSC":
+                    continue
+                if record["addr"] in existing_addrs:
+                    print("Skipped:",record["addr"])
+                    continue
 
-            address = Address(record["addr"])
-            init_args = dict(
-                name=record["name"],
-                symbol=record["symbol"],
-                address=address,
-                block_time=int(record["blockTime"]),
-                updated=int(datetime.now().timestamp())
-            )
-            
-            # BscScan
-            updt = backoff(bscscan.get,address)
-            if updt is None:
-                continue
-            init_args.update(updt)
+                address = Address(record["addr"])
+                init_args = dict(
+                    name=record["name"],
+                    symbol=record["symbol"],
+                    address=address,
+                    block_time=int(record["blockTime"]),
+                    updated=int(datetime.now().timestamp())
+                )
+                
+                # BscScan
+                updt = backoff(bscscan.get,address)
+                if updt is None:
+                    continue
+                init_args.update(updt)
 
-            # BSCheck
-            init_args.update(bscheck.get(address))
+                # BSCheck
+                init_args.update(bscheck.get(address))
+                
+                # Token Sniffer
+                init_args.update(tokensniffer.get(address))
+                
+                Token(**init_args).insert_or_update(db=self.db)
+                print(f"{i+1}/{data_len}")
+                self.db.conn.commit()
+
+                if time() - cycle_start > 120:
+                    break
             
-            # Token Sniffer
-            init_args.update(tokensniffer.get(address))
-            
-            Token(**init_args).insert_or_update(db=self.db)
-            print(f"{i+1}/{data_len}")
-            self.db.conn.commit()
+            if time() - cycle_start < 5:
+                print("Closed")
+                break
 
         self.db.close()
