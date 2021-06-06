558b8d4697788a3eb0c96ec1421e7fde335b3f89
Obaid26 <51287329+Obaid26@users.noreply.github.com>
2021-06-05 19:02:03 -0700

    Not including external for now

--- commands/migrate.py
+++ commands/migrate.py
@@ -1,13 +1,18 @@
-from core.Token.CoreToken import CoreToken
+from library.postgres import DB
+from library.BaseModel import BaseModel
+
+# Models
+from core.Token.Token import Token
+from core.Token.TokenMeta import TokenMeta
+from core.Token.BSCheckRating import BSCheckRating
 from core.Holders.Holders import Holders
 from core.misc.Proxies import Proxies
 from core.misc.TokenRequest import TokenRequest
 from core.misc.Listing import Listing
-from library.postgres import DB
 
 db = DB("tokens")
 
-for _class in [CoreToken,Holders,Proxies,TokenRequest,Listing]:
+for _class in BaseModel.__subclasses__():
     print(_class)
     print("New Columns:",_class._db_new_cols())
 
--- core/Token/ViewableToken.py
+++ core/Token/ViewableToken.py
@@ -7,54 +7,17 @@ class ViewableToken(BaseModel):
         address:Address,
         name:str,
         symbol:str,
-        
-        listings:str,
 
         source_verified:bool,
         block_time:int,
         holders:int,
 
-        bscheck_rating:str,
-        honeypot_check:bool,
-        owner_renounced:bool,
-        dev_liquidity_check:bool,
-        lp_check:bool,
-        top_holders_check:bool,
-
-        no_older_tokens:bool,
-        not_proxy:bool,
-        not_pausable:bool
+        listings:str,
     ) -> None:
         pass
 
-        
-    
-    @classmethod
-    def get(cls,address:Address):
-        address = str(Address(address))
-        query = """
-        SELECT
-            tokens.address,
-            tokens.name,
-            tokens.symbol,
-            string_agg(listings.platform, ',') AS listings,
-            token_meta.source_verified AS source_verified,
-            token_meta.holders AS holders,
-            token_meta.block_time AS block_time,
-        FROM tokens
-        JOIN listings ON tokens.address = listings.token
-        JOIN token_meta ON tokens.address = token_meta.address
-        WHERE tokens.address = %s
-        GROUP BY tokens.address
-        """
-        with DB("tokens") as db:
-            return cls._from_row(db.get(query,[address]))
-
-    @classmethod
-    def search(cls,keyword,limit=10):
-        limit_cond = cls.limit_cond(limit)
-        lkey = keyword.lower()
-        placeholder = DB.placeholder(1)
+    @staticmethod
+    def _build_query(where:str):
         query = f"""
         SELECT
             tokens.address,
@@ -67,12 +30,42 @@ class ViewableToken(BaseModel):
         FROM tokens
         JOIN listings ON tokens.address = listings.token
         JOIN token_meta ON tokens.address = token_meta.address
+        {where}
+        GROUP BY tokens.address
+        """
+        return query
+
+    
+    @classmethod
+    def get(cls,address:Address):
+        address = str(Address(address))
+        query = cls._build_query(f"WHERE tokens.address = {DB.placeholder(1)}")
+        with DB("tokens") as db:
+            return cls._from_row(db.get(query,[address]))
+
+    @classmethod
+    def search(cls,keyword,limit=10):
+        limit_cond = cls.limit_cond(limit)
+        lkey = keyword.lower()
+        placeholder = DB.placeholder(1)
+        query = cls._build_query(f"""
         WHERE tokens.address = {placeholder}
         OR LOWER(tokens.symbol) LIKE {placeholder}
         OR LOWER(tokens.name) LIKE {placeholder}
-        GROUP BY tokens.address
+        """)
+        query += limit_cond
+        with DB("tokens") as db:
+            rows = db.get_all(query,[keyword,*[f"%{lkey}%"] * 2])
+            return [cls._from_row(row) for row in rows]
+    
+    @classmethod
+    def get_latest(cls,limit=100):
+        limit_cond = cls.limit_cond(limit)
+        query = cls._build_query()
+        query += f"""
+        ORDER BY token_meta.block_time DESC
         {limit_cond}
         """
         with DB("tokens") as db:
-            rows = db.get_all(query,[keyword,*[f"%{lkey}%"] * 2])
+            rows = db.get_all(query)
             return [cls._from_row(row) for row in rows]
