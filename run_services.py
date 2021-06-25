2366317bab8d1d9c3a7ab3b58229e0ceeba43019
Obaid26 <51287329+Obaid26@users.noreply.github.com>
2021-06-24 21:59:41 -0700

    HEY

--- run_services.py
+++ run_services.py
@@ -55,8 +55,8 @@ if __name__ == "__main__":
             # services.poll_verified,
             # services.sweep_verified,
             # services.pull_listing_tokens,
-            services.update_all_holders,
-            # services.pull_token_prices
+            # services.update_all_holders,
+            services.pull_token_prices
         ]
 
     for module in threads_to_run:
