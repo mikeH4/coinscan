7afd3aa60e0656d3b82237a89b1771ee46ed0936
Obaid26 <51287329+Obaid26@users.noreply.github.com>
2021-06-27 01:41:47 -0700

    C

--- services/sweep_pair_holders.py
+++ services/sweep_pair_holders.py
@@ -9,29 +9,32 @@ def main():
     from library.postgres import DB
 
     with DB("tokens") as db:
-        repeater = Repeater(min=60*10)
+        repeater = Repeater(min=60*5)
         bscscan = BscScan()
 
         while repeater.loop():
-            pairs = Pairs.unknown_pairs(db=db,limit=100)
-            pairs_len = len(pairs)
-
-            for i,address in enumerate(pairs):
-                total,top = bscscan.holders(
-                    address=address
-                )
-                TokenMeta.update(
-                    address=address,
-                    db=db,
-                    holders=total
-                )
-                Holders.delete_all(contract=address,db=db)
-                for holder,address_info in top:
-                    holder.insert_or_update(db=db)
-                    address_info.insert(db=db,replace=True)
-
-                db.conn.commit()
-                print(f"{i+1}/{pairs_len} Pair Holders Updated")
-
-                if repeater.should_repeat():
+            while True:
+                pairs = Pairs.unknown_pairs(db=db,limit=100)
+                pairs_len = len(pairs)
+                if pairs_len < 1:
                     break
+
+                for i,address in enumerate(pairs):
+                    total,top = bscscan.holders(
+                        address=address
+                    )
+                    TokenMeta.update(
+                        address=address,
+                        db=db,
+                        holders=total
+                    )
+                    Holders.delete_all(contract=address,db=db)
+                    for holder,address_info in top:
+                        holder.insert_or_update(db=db)
+                        address_info.insert(db=db,replace=True)
+
+                    db.conn.commit()
+                    print(f"{i+1}/{pairs_len} Pair Holders Updated")
+
+                    if repeater.should_repeat():
+                        break
