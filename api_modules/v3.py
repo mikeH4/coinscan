b9f526230c07058db826965cf637d92328e6e673
Obaid26 <email@example.com>
2021-07-08 14:39:35 -0700

    A

--- api_modules/v3.py
+++ api_modules/v3.py
@@ -15,14 +15,9 @@ routers = [
 
 for router in routers:
     @router.get("/")
-    def index():
-        return {
-            "actions": [route.path for route in router.routes]
-        }
+    def index(): return dict(action=[route.path for route in router.routes])
+    
     app.include_router(router)
 
 @app.get("/")
-async def root_index():
-    return {
-        "actions": [router.prefix for router in routers]
-    }
+def root_index(): return dict(actions=[router.prefix for router in routers])
