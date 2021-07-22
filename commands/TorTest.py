af3dfea780369a3a8f3a68e512683e82e62bc95b
Obaid26 <email@example.com>
2021-07-21 17:46:45 -0700

    Testing

--- commands/TorTest.py
+++ commands/TorTest.py
@@ -1,12 +1,12 @@
 from concurrent.futures.thread import ThreadPoolExecutor
 from core.sources.Test import IfConfig
 from library.timer import timer
-from library.BaseSource import RequestPool, TorRequestPool
+from library.BaseSource import RequestPool
 RequestPool._init_proxies()
 
 ip = IfConfig()
 
-test_size = 5
+test_size = 200
 
 with timer("Tor Pool") as increment:
     with ThreadPoolExecutor(max_workers=20) as exec:
@@ -15,7 +15,7 @@ with timer("Tor Pool") as increment:
     increment(test_size)
 
 with timer("Proxy Pool") as increment:
-    ip.request_manager = TorRequestPool
+    ip.request_manager = RequestPool
     with ThreadPoolExecutor(max_workers=20) as exec:
         for i in range(test_size):
             exec.submit(ip.get)
