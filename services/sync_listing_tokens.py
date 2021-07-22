5fd228cc83b56c3b1f5e202fbb141005aa74d320
Obaid26 <email@example.com>
2021-07-22 07:37:05 -0700

    Extosy

--- services/sync_listing_tokens.py
+++ services/sync_listing_tokens.py
@@ -8,29 +8,30 @@ from library.postgres import DB
 bscscan_api = BscScanApi()
 scanner_api = ScannerApi()
 
-with DB() as db:
-    addresses = Listing.get_addresses_not_inserted(db=db)
-    addresses_len = len(addresses)
-    print(f"Search for {addresses_len} addresses")
+def main():
+    with DB() as db:
+        addresses = Listing.get_addresses_not_inserted(db=db)
+        addresses_len = len(addresses)
+        print(f"Search for {addresses_len} addresses")
 
-    data = scanner_api.get_addresses(addresses)
-    data_len = len(data)
+        data = scanner_api.get_addresses(addresses)
+        data_len = len(data)
 
-    print(f"{addresses_len-data_len} tokens not in response")
+        print(f"{addresses_len-data_len} tokens not in response")
 
-    for i,token_data in enumerate(data):
-        address = Address(token_data["address"])
+        for i,token_data in enumerate(data):
+            address = Address(token_data["address"])
 
-        decimals = token_data["decimals"]
-        total_supply = token_data["total_supply"]/(10**decimals)
-        Token.insert_with_source(
-            bscscan_api=bscscan_api,
-            address=address,
-            name=token_data["name"],
-            symbol=token_data["symbol"],
-            decimals=decimals,
-            total_supply=total_supply,
-            db=db
-        )
+            decimals = token_data["decimals"]
+            total_supply = token_data["total_supply"]/(10**decimals)
+            Token.insert_with_source(
+                bscscan_api=bscscan_api,
+                address=address,
+                name=token_data["name"],
+                symbol=token_data["symbol"],
+                decimals=decimals,
+                total_supply=total_supply,
+                db=db
+            )
 
-        print(f"{i+1}/{data_len} Token Inserted")
+            print(f"{i+1}/{data_len} Token Inserted")
