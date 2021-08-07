cdb8f52f8cb04f85a3e82369a81cd58c86cb8ff8
Obaid26 <email@example.com>
2021-08-06 22:36:33 -0700

    F

--- services/poll_new.py
+++ services/poll_new.py
@@ -31,6 +31,8 @@ def main():
                     # Just so memory doesn't escape
                     existing_addrs = existing_addrs[5000:]
 
+                print(str(a) for a in addresses)
+                print(str(a) for a in existing_addrs)
                 # assert len(set(addresses) - set(existing_addrs)) == (len(addresses) - len(existing_addrs))
 
                 for i,token_data in enumerate(data):
