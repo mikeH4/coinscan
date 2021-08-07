45e4e0d52ade7edcc8a72ac45ac8e4a982d4ecf1
Obaid26 <email@example.com>
2021-08-06 22:19:02 -0700

    A

--- services/poll_new.py
+++ services/poll_new.py
@@ -31,8 +31,9 @@ def main():
                     # Just so memory doesn't escape
                     existing_addrs = existing_addrs[5000:]
 
-                assert len(set(addresses) - set(existing_addrs)) == (len(addresses) - len(existing_addrs))
                 print("subtracted",len(set(addresses) - set(existing_addrs)))
+                print("minused",(len(addresses) - len(existing_addrs)))
+                assert len(set(addresses) - set(existing_addrs)) == (len(addresses) - len(existing_addrs))
 
                 for i,token_data in enumerate(data):
                     address = AddressHash(token_data["address"])
